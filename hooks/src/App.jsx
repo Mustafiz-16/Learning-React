bca0fe68abee47c7dcee2c8d8f44293cd7b77ba0
mustafiz <mustafizurrahaman362@gmail.com>
2025-09-30 22:41:19 +0600

    update: setup tailwind

--- hooks/src/App.jsx
+++ hooks/src/App.jsx
@@ -1,7 +1,13 @@
-return (
-    <> 
-    </>
+
+
+import React from 'react'
+
+export default function App() {
+  return (
+    <div>
+      <h1 className='text-2xl'>Hello world</h1>
+      
+    </div>
   )
+}
 
-
-export default App
